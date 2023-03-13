f8f438a8b51f295fc42ce65748cfa7328e9abb51
mquevedo <mquevedo@unicauca.edu.co>
2023-03-12 22:25:20 -0500

    intento de agregar carpeta a git

---
+++ draw_mouse/eventos.js
@@ -0,0 +1,49 @@
+var frame = document.getElementById("drawArea");
+var paper = frame.getContext("2d");
+var borderColor = "black";
+var btnBlack = document.getElementById("btnCBlack");
+var btnBlue = document.getElementById("btnCBlue");
+var btnYellow = document.getElementById("btnCYellow");
+var btnWhite = document.getElementById("btnCWhite");
+var slider = document.getElementById("slider");
+let mouse = false;
+console.log("valor slider "+slider.value);
+frame.height = slider.value;
+frame.width = slider.value;
+frame.addEventListener("mousedown", clickMouse);
+frame.addEventListener("mouseup", noClickMouse);
+frame.addEventListener("mousemove", drawMouse);
+btnBlack.addEventListener("click", console.log("presionaste el boton negro"));
+btnBlue.addEventListener("click", drawMouse("#008CBA"));
+btnYellow.addEventListener("click", console.log("presionaste el boton yellow"));
+btnWhite.addEventListener("click", drawMouse("white"));
+
+//bordes de canvas
+drawLine(borderColor, 1, 1, 1, frame.height-1, paper);//left vertical line
+drawLine(borderColor, frame.width-1, 1, frame.width-1, frame.height-1, paper);//right vertical line
+drawLine(borderColor, 1, 1, frame.width-1, 1, paper);//up horizontal line
+drawLine(borderColor, 1, frame.height-1, frame.width-1, frame.height-1, paper);//down horizontal line
+
+function clickMouse(){
+    mouse = true;
+}
+
+function noClickMouse(){
+    mouse = false;
+}
+
+function drawMouse(color){
+    if(mouse == true){
+        drawLine(color, event.layerX-1, event.layerY-1, event.layerX+1, event.layerY+1, paper);
+    }
+}
+
+function drawLine(color, xi, yi, xf, yf, lienzo){
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
+    lienzo.moveTo(xi, yi);
+    lienzo.lineTo(xf,yf);
+    lienzo.stroke();
+    lienzo.closePath();
+}
